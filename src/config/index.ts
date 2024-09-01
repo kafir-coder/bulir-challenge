0c4b2c3a746346dd7ca9b7864bbe11c2a899f0b8
caiotony16@gmail.com <Caio Tony>
2024-09-01 19:43:22 +0100

    chore: add rate limiter

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,3 +4,4 @@ dotenv.config()
 
 export const jwt_secret = process.env.JWT_SECRET || ''
 export const token_expiration_time = process.env.JWT_EXPIRATION || ''
+export const max_request_per_second = process.env.MAX_REQUEST_PER_SEC || ''
--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import { authHandlers } from './api/auth'
 import errorHandler from './common/middleware/error-handling'
 import authenticateJWT from './utils/http/middleware/jwt'
 import helmet from 'helmet'
+import { rateLimiter } from './common/middleware/rate-limiter'
 const app = express()
 
 start().catch((e) => {
@@ -16,6 +17,7 @@ start().catch((e) => {
 app.use(helmet())
 app.use(express.json())
 app.use(morgan('combined'))
+app.use(rateLimiter)
 
 app.use('/auth', authHandlers)
 app.use('/services', authenticateJWT, serviceBookingHandlers)
