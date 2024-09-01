d098d80b66b613b8460f780982c8d274523fc1da
caiotony16@gmail.com <Caio Tony>
2024-09-01 20:41:03 +0100

    ref: use const instead

--- src/modules/auth/auth.ts
+++ src/modules/auth/auth.ts
@@ -2,16 +2,13 @@ import { jwt_secret, token_expiration_time } from '../../config'
 import { IAuthSvc } from '../../interfaces/auth'
 import { LoginDto } from '../../models/auth'
 import { IUserRepo } from '../user/repository/repository'
-import * as bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 import * as jwt from 'jsonwebtoken'
 import { Unauthorized } from '../../common/errors/unathourized'
 import { RequestContext } from '../../models/request'
 export class AuthSvc implements IAuthSvc {
   constructor(private readonly userRepo: IUserRepo) {}
-  async login(
-    _ctx: RequestContext,
-    params: LoginDto,
-  ): Promise<Record<string, any>> {
+  async login(_ctx: RequestContext, params: LoginDto) {
     const user = await this.userRepo.getByEmail(params.email)
 
     if (!user) {
@@ -20,12 +17,14 @@ export class AuthSvc implements IAuthSvc {
 
     const isPasswordValid = await bcrypt.compare(params.password, user.password)
     if (!isPasswordValid) {
-      throw new Unauthorized('kkkkk')
+      throw new Unauthorized('Password is not valid')
     }
 
     const payload = { id: user.id, email: user.email, role: user.role }
 
-    const token = jwt.sign(payload, jwt_secret, { expiresIn: '1h' })
+    const token = jwt.sign(payload, jwt_secret, {
+      expiresIn: token_expiration_time,
+    })
 
     return {
       accessToken: token,
