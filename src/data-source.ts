778018b901cc0bfd979a55297db24e561b57088e
caiotony16@gmail.com <Caio Tony>
2024-09-01 18:35:37 +0100

    chore: add left files

---
+++ src/data-source.ts
@@ -0,0 +1,16 @@
+import { DataSource } from 'typeorm' // Example entity, replace with your own
+import { User } from './models/user'
+import { Service, ServiceBooking } from './models/service'
+import dotenv from 'dotenv'
+dotenv.config()
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  entities: [User, Service, ServiceBooking], // Add your entities here
+  synchronize: true, // For development; disable in production
+  logging: false,
+})
---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { start } from './api/api'
+import express from 'express'
+import { serviceBookingHandlers } from './api/service-booking'
+import morgan from 'morgan'
+import { userHandlers } from './api/user'
+import { authHandlers } from './api/auth'
+import errorHandler from './common/middleware/error-handling'
+import authenticateJWT from './utils/http/middleware/jwt'
+const app = express()
+
+start().catch((e) => {
+  console.log(e)
+})
+
+app.use(express.json())
+app.use(morgan('combined'))
+
+app.use('/auth', authHandlers)
+app.use('/services', authenticateJWT, serviceBookingHandlers)
+app.use('/users', userHandlers)
+
+app.use(errorHandler)
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`App listening on port ${process.env.PORT || 8080}`)
+})
